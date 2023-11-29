72722838ae266b84ab37926bec136ab63f9f451f
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-29 14:31:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import './components/AppHeader.vue';
+import './components/AppMain.vue'
+import {store} from './store'
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './style/general.scss';
+</style>
